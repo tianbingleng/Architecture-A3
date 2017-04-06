6e1899c99a9d8a7cc3316297c16804a36ca33999
JunyuanZhang <junyuanz@andrew.cmu.edu>
2017-04-05 22:54:35 -0400

    Modify GetConnection

--- EEPs/InventoryMgr/src/Middleware/GetConnection.java
+++ EEPs/InventoryMgr/src/Middleware/GetConnection.java
@@ -13,6 +13,8 @@ import java.sql.DriverManager;
  * @author socyrus
  */
 public class GetConnection {
+    String username = "remote";
+    String password = "remote_pass";
     /* @return true get connected to the database
       * @return false get error while connecting
       * @param databaseIP The IP of database
@@ -22,7 +24,7 @@ public class GetConnection {
      private boolean getConnection(Connection dbConn, String databaseIP, String databaseName, String errorMsg){
          String sourceURL = "jdbc:mysql://" + databaseIP + ":3306/"+databaseName;
          try{
-             dbConn = DriverManager.getConnection(sourceURL,"remote","remote_pass");
+             dbConn = DriverManager.getConnection(sourceURL,username, password);
          }
          catch(Exception e){
              errorMsg = e.getMessage();
