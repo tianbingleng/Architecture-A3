d48821579dc8a3c38b1558251de63e090894a84a
JunyuanZhang <junyuanz@andrew.cmu.edu>
2017-04-07 14:00:15 -0400

    Modify main method

--- EEPs/InventoryMgr/src/InventoryMainFrame.java
+++ EEPs/InventoryMgr/src/InventoryMainFrame.java
@@ -673,9 +673,9 @@ public class InventoryMainFrame extends MyFrame {
     * @param args the command line arguments
     */
     public static void main(String args[]) {
-        java.awt.EventQueue.invokeLater(new Runnable() {
+       java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new InventoryMainFrame().setVisible(true);
+                new LoginFrame("inventory", new InventoryMainFrame()).setVisible(true);
             }
         });
     }
