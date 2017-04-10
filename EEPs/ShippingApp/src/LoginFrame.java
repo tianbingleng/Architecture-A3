bb4630e04c73629122ce4b0666414a5188fd573a
JunyuanZhang <junyuanz@andrew.cmu.edu>
2017-04-10 14:10:34 -0400

    Useless LoginFrame main method deleted

--- EEPs/ShippingApp/src/LoginFrame.java
+++ EEPs/ShippingApp/src/LoginFrame.java
@@ -191,11 +191,7 @@ public class LoginFrame extends javax.swing.JFrame {
 
         /* Create and display the form */
         
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new LoginFrame("order", new NewJFrame()).setVisible(true);
-            }
-        });
+       
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
